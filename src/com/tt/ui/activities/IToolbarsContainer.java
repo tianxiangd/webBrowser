efaab4609cf0cf4c4d61f0cd9552f7c1b8230ab9
Tee <tianxiangdong32@sian.com>
2016-04-22 14:50:57 +0800

    commit

---
+++ src/com/tt/ui/activities/IToolbarsContainer.java
@@ -0,0 +1,15 @@
+
+
+package com.tt.ui.activities;
+
+/**
+ * Interface defining a tool bar container.
+ */
+public interface IToolbarsContainer {
+	
+	/**
+	 * Hide the tool bars of this item.
+	 */
+	void hideToolbars();
+
+}
